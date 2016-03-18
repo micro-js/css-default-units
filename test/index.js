4d581db1c3feaafef5a98b4ec1465b77db992c5a
Andrew Shaffer <andrew@weo.io>
2016-03-17 19:57:49 -0700

    handle empty string and null correctly

--- test/index.js
+++ test/index.js
@@ -12,6 +12,8 @@ var test = require('tape')
 test('should work', function (t) {
   t.equal(defaultUnits('height', 50), '50px')
   t.equal(defaultUnits('opacity', 1), 1)
-  
+  t.equal(defaultUnits('display', ''), '')
+  t.equal(defaultUnits('display', null), null)
+
   t.end()
 })
